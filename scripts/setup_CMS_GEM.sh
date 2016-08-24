0166803d1709a1717b0de2d6013019c02596b0b3
Brian Dorney <brian.l.dorney@cern.ch>
2016-08-24 13:36:30 +0200

    Adding mergeSelectedRuns.sh for use in Grid Mode

--- scripts/setup_CMS_GEM.sh
+++ scripts/setup_CMS_GEM.sh
@@ -7,20 +7,9 @@ export GEM_BASE=$PWD
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.8/x86_64-slc6/setup.sh
 #source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.2/x86_64-slc6/setup.sh
 
-#export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/6.00.02/x86_64-slc6-gcc48-opt/root
-cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.00.02/x86_64-slc6-gcc48-opt/root
+#cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.00.02/x86_64-slc6-gcc48-opt/root
+cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.06/x86_64-slc6-gcc48-opt/root
 source bin/thisroot.sh
-#export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/6.02.08/x86_64-slc6-gcc49-opt/root
-
-#export PATH=${PATH}:$ROOTSYS/bin
-#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ROOTSYS/lib64
-#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ROOTSYS/lib
-#export LD_LIBRARY_PATH=$ROOTSYS/lib64
-
-#alias root=$ROOTSYS/bin/root
-#alias root-config=$ROOTSYS/bin/root-config
-#alias rootcint=$ROOTSYS/bin/rootcint
-#alias rootcling=$ROOTSYS/bin/rootcling
 
 cd $GEM_BASE
 
