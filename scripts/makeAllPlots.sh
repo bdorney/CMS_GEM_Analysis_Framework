7e0ffd075046d20833817be08bc5bf1bdc7cf057
Brian Dorney <brian.l.dorney@cern.ch>
2016-10-20 19:48:10 +0200

    new helper script

---
+++ scripts/makeAllPlots.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+#Usage:
+#	source makeAllPlots.sh <Plot Config File Directory>
+
+#store the original directory
+DIR_ORIG=$PWD
+
+#setup input variables
+DIR_CONFIG=$1
+
+#Move to the data directory
+cd $DIR_CONFIG
+
+#Make Plots
+for f in *.cfg
+do
+	echo "=============================================="
+	echo "Making plot from file:" $f
+	$GEM_BASE/genericPlotter $f false
+done
+
+echo "=============================================="
+echo "All Plots Have Been Made!"
+
+echo "Your plots will be found in:"
+echo ""
+echo "\t"$DIR_CONFIG
+echo ""
+
+#return to the original directory
+cd $DIR_ORIG
