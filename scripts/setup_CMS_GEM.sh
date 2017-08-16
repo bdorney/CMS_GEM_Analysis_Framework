6d8282a77be7fd48243ced9ba4a18d7765d70e35
Anastasia <anastasia.kotsokechagia@cern.ch>
2017-08-16 11:20:44 +0200

    Add xlrd library in pip install command and removed all the commented lines

--- scripts/setup_CMS_GEM.sh
+++ scripts/setup_CMS_GEM.sh
@@ -7,18 +7,14 @@ export GEM_BASE=$PWD
 
 #Setup gcc/g++
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.8/x86_64-slc6/setup.sh
-#source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.2/x86_64-slc6/setup.sh
 
 #Setup ROOT
-#cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.00.02/x86_64-slc6-gcc48-opt/root
 cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.06/x86_64-slc6-gcc48-opt/root
-#cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc49-opt/root
 source bin/thisroot.sh
 
 cd $GEM_BASE
 
 #Setup Python
-#export PYTHONDIR=/afs/cern.ch/sw/lcg/external/Python/2.7.3/x86_64-slc6-gcc48-opt
 export PYTHONDIR=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt
 export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$PYTHONDIR/lib:$LD_LIBRARY_PATH:/opt/rh/python27/root/usr/lib64
@@ -35,26 +31,15 @@ if [[ !  -d $DIR_PIP ]]; then
 	export PYTHONPATH=$DIR_PIP/lib/python2.7/site-packages:$PYTHONPATH
 	export PATH="$PATH:~/.local/bin"
 	alias pip2.7="$DIR_PIP/bin/pip"
-	
-	#echo "-----"
-	#echo "ls $DIR_PIP/bin/pip"
-	#ls $DIR_PIP/bin/pip
-	#echo "pip2.7 = " 
-	#which pip2.7
-	#echo "-----"
 
 	#Installing packages
-	#pip2.7 install --user numpy scipy matplotlib ipython jupyter pandas sympy nose root_numpy xlrd
-	$DIR_PIP/bin/pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose root_numpy
+	$DIR_PIP/bin/pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose root_numpy xlrd
 else
 	#Update environment	
 	export PYTHONPATH=$DIR_PIP/lib/python2.7/site-packages:$PYTHONPATH
 	export PATH="$PATH:~/.local/bin"
 	alias pip2.7="$DIR_PIP/bin/pip"
 fi
-
-#alias helper scripts
-#alias makePlots="source $GEM_BASE/scripts/makeAllPlots.sh"
 
 which root
 which python2.7
